144c6927816c5163ed1a6cdc1fc66908d0709aae
mahdi <aghamohammadi.mehdi15@gmail.com>
2022-06-11 10:35:02 +0430

    add swagger and generate api document

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,6 +13,47 @@ use \Exception;
 
 class AuthController extends Controller
 {
+
+    /**
+     * @OA\Post(
+     * path="/api/register",
+     * operationId="Register",
+     * tags={"Register"},
+     * summary="User Register",
+     * description="User Register here",
+     *     @OA\RequestBody(
+     *         @OA\JsonContent(),
+     *         @OA\MediaType(
+     *            mediaType="multipart/form-data",
+     *            @OA\Schema(
+     *               type="object",
+     *               required={"name","email", "password", "password_confirmation"},
+     *               @OA\Property(property="name", type="text"),
+     *               @OA\Property(property="email", type="text"),
+     *               @OA\Property(property="password", type="password"),
+     *               @OA\Property(property="password_confirmation", type="password")
+     *            ),
+     *        ),
+     *    ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Register Successfully",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Register Successfully",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Unprocessable Entity",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(response=400, description="Bad request"),
+     *      @OA\Response(response=404, description="Resource Not Found"),
+     * )
+     */
     public function register(RegisterRequest $request)
     {
         try {
@@ -31,6 +72,44 @@ class AuthController extends Controller
         }
     }
 
+    /**
+     * @OA\Post(
+     * path="/api/login",
+     * operationId="authLogin",
+     * tags={"Login"},
+     * summary="User Login",
+     * description="Login User Here",
+     *     @OA\RequestBody(
+     *         @OA\JsonContent(),
+     *         @OA\MediaType(
+     *            mediaType="multipart/form-data",
+     *            @OA\Schema(
+     *               type="object",
+     *               required={"email", "password"},
+     *               @OA\Property(property="email", type="email"),
+     *               @OA\Property(property="password", type="password")
+     *            ),
+     *        ),
+     *    ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Login Successfully",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Login Successfully",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Unprocessable Entity",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(response=400, description="Bad request"),
+     *      @OA\Response(response=404, description="Resource Not Found"),
+     * ),
+     */
     public function login(LoginRequest $request)
     {
         if (Auth::attempt($request->only('email', 'password'))) {
@@ -44,11 +123,41 @@ class AuthController extends Controller
         ]);
     }
 
+    /**
+     * @OA\Post(
+     * path="/api/logout",
+     * operationId="Logout User",
+     * tags={"Logout"},
+     * summary="User Logout",
+     * description="Logout User Here",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Logout Successfully",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(response=401, description="You Must Be First Login"),
+     * )
+     */
     public function logout()
     {
         Auth::user()->tokens()->delete();
     }
 
+    /**
+     * @OA\Get(
+     * path="/api/authenticate",
+     * summary="Check The User Is Authenticate",
+     * description="Check The User Is Authenticate",
+     * tags={"UserAuthenticate"},
+     * security={ {"bearer": {} }},
+     *      @OA\Response(
+     *          response=200,
+     *          description="User Is Authenticate",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(response=401, description="You Must Be First Login"),
+     * )
+     */
     public function authenticate()
     {
         if (Auth::check()) {
